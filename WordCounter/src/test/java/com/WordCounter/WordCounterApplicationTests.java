e309ed469dab5d8432844e75341d0f1b8d5260aa
BennyTal <Benny.tapan@Gmail.com>
2020-07-27 17:35:54 +0300

    Java spring + Spark

---
+++ WordCounter/src/test/java/com/WordCounter/WordCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WordCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
